0d0b989a1b5b843ac560e0fc16c62c20d1cdcd8e
Luke Bemish <lukebemish@lukebemish.dev>
2023-03-13 23:48:39 -0500

    Update docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -9,9 +9,8 @@ makedocs(
     modules = [Hrse]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo   = "github.com/lukebemish/Hrse.jl.git",
+    target = "build",
+    push_preview = true
+)
